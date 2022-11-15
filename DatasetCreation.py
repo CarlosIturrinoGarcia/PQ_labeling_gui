ddde9f57001f4ceccc1668a2e2640cadcd24fcb1
CarlosIturrinoGarcia <carlos.iturrinogarcia@unifi.it>
2022-11-15 12:44:20 +0100

    insert comments in DatasetCreation.py

--- DatasetCreation.py
+++ DatasetCreation.py
@@ -1,10 +1,16 @@
 import numpy as np
 
 def sig2matrix(sig):
+    """"
+    Create signal matrix for Training
+    """
     siglength = len(sig)
     matcolumnsize = round(siglength/18)
     return matcolumnsize
 
 def conversion(region):
+    """"
+    Create the index in the signal matrix
+    """
     print(np.shape(region))
     print(region)
