2a175b55060bfe2e2de29709fb7f0daa15777363
CarlosIturrinoGarcia <carlos.iturrinogarcia@unifi.it>
2022-11-15 12:28:27 +0100

    add sig2matrix function

--- DatasetCreation.py
+++ DatasetCreation.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+def sig2matrix(sig):
+    siglength = len(sig)
+    matcolumnsize = round(siglength/18)
+    return matcolumnsize
 
 def conversion(region):
     print(np.shape(region))
