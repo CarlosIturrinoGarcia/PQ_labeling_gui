05c01e7ec70956bd74f610ca6e4c56fce9a41689
CarlosIturrinoGarcia <carlos.iturrinogarcia@unifi.it>
2023-01-10 11:25:08 +0100

    input to the conversion function test region in the matrix

--- DatasetCreation.py
+++ DatasetCreation.py
@@ -1,19 +1,22 @@
 import numpy as np
 
+
 def sig2matrix(sig):
     """"
     Create signal matrix for Training
     """
     siglength = len(sig)
-    matcolumnsize = round(siglength/18)
+    matcolumnsize = round(siglength / 18)
     num_classes = 6
-    matrix = np.zeros((num_classes,matcolumnsize))
+    matrix = np.zeros((num_classes, matcolumnsize))
     return matrix
 
-def conversion(region):
+
+def conversion(region, matrix):
     """"
     Create the index in the signal matrix
     """
-
+    region2matrix = round(region / 18)
+    matrix[1, region2matrix] = 1
     print(np.shape(region))
     print(region)
